655901bf5145a49f0302af9a3f1ef38624031cf9
Unknown <hugo.maitre69@gmail.com>
2018-10-13 17:08:09 +0200

    Creation of the API folder structure

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const path = require('path');
-const router = express.Router();
+const express = require('express')
+const path = require('path')
+const router = express.Router()
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 
-router.get("/hello", (req, res) => {
-    res.status(200).json("Hello world !");
-})
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
 
+router.use(require("./routes"))
 
 router.all("*", (req, res) => {
-    res.sendStatus(404);
-    res.end();
+    res.sendStatus(404)
+    res.end()
 })
 
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-require("dotenv").config();
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 5000;
+require("dotenv").config()
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 5000
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client', 'build')));
+    app.use(express.static(path.join(__dirname, 'client', 'build')))
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    });
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
 }
-app.options("*", (req, res) => {
-    var headers = {};
+
+
+app.use("*", (req, res, next) => {
+    var headers = {}
     // IE8 does not allow domains to be specified, just the *
-    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-    headers["Access-Control-Allow-Origin"] = "*";
-    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-    headers["Access-Control-Allow-Credentials"] = false;
-    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-    res.writeHead(200, headers);
-    res.end();
+    // headers["Access-Control-Allow-Origin"] = req.headers.origin
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS"
+    headers["Access-Control-Allow-Credentials"] = false
+    headers["Access-Control-Max-Age"] = '86400' // 24 hours
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+    if(req.method ===  'OPTIONS') {
+        res.writeHead(200, headers)
+        res.end()
+    } else {
+        next()
+    }
 })
 
+
 // API calls
-app.use('/api', require("./api"));
+app.use('/api', require("./api"))
 
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`))
