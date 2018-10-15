2845014ac546b4c3e6fba909b61d138346e2ea6c
Hugo Maitre <hugo.maitre69@gmail.com>
2018-10-15 23:03:15 +0200

    Add things

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { simpleAction } from './redux/actions/simpleAction'
+import createHistory from 'history/createBrowserHistory'
+ 
+const history = createHistory()
 
 class App extends Component {
 
@@ -13,41 +16,48 @@ class App extends Component {
 
     render() {
         return (
-            <BrowserRouter>
-            <div className="App">
-                <Link to="/login">Home</Link>{' '}
-                <Link to={{pathname: '/'}}>About</Link>{' '}
-                <Link to="/contact">Contact</Link>
-                <Switch>
-                    <Route path="/login">
-                        <div>
-                            Mdr salut !
-                        </div>
-                    </Route>
-                    <Route path= "">
-                        <div>
-                        <button onClick={this.simpleAction}>Test redux action</button>
-                        <pre>
-                        {
-                        JSON.stringify(this.props)
-                        }
-                        </pre>
-                            <header className="App-header">
-                                <img src={logo} className="App-logo" alt="logo" />
-                                <h1 className="App-title">Welcome to React</h1>
-                            </header>
-                            <p className="App-intro">Salut</p>
-                        </div>
-                    </Route>
-                </Switch>
+            <BrowserRouter history={history}>
+                <div className="App">
+                    <Link to="/login">Home</Link>{' '}
+                    <Link to={{pathname: '/'}}>About</Link>{' '}
+                    <Link to="/contact">Contact</Link>
+                    <Switch>
+                        <Route path="/login">
+                            <div>
+                                Mdr salut !
+                            </div>
+                        </Route>
+                        <Route path= "">
+                            <div>
+                            <button onClick={this.simpleAction}>Test redux action</button>
+                            <pre>
+                            {
+                            JSON.stringify(this.props)
+                            }
+                            </pre>
+                                <header className="App-header">
+                                    <img src={logo} className="App-logo" alt="logo" />
+                                    <h1 className="App-title">Welcome to React</h1>
+                                </header>
+                                <p className="App-intro">Salut</p>
+                            </div>
+                        </Route>
+                    </Switch>
                 </div>
             </BrowserRouter>
         );
     }
 }
-export default connect(state => {
-    console.log({...state})
-    return ({
-    ...state
-   })
-})(App)
+export default connect(
+    state =>
+        ({
+        ...state
+    })
+    ,
+    dispatch => {
+        return {
+            simpleAction: () => {
+                dispatch(simpleAction())
+            }
+        }
+    })(App)
