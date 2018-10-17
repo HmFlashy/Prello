6180289bf542dd4394cac566b5b16216b8832794
Hugo Maitre <hugo.maitre69@gmail.com>
2018-10-17 09:54:58 +0200

    Some change in routes and the card controller

--- api/routes/cards/getCardById.js
+++ api/routes/cards/getCardById.js
@@ -1,7 +1,7 @@
 const CardController = require('../../controllers/cardsController')
 
 module.exports = (req, res) => {
-    CardController.getCardById(req).then((data) => {
+    return CardController.getCardById(req.params.id).then((data) => {
         res.status(201).json(data)
     }).catch((err) => {
         res.status(err.code).json(err.message)
--- api/routes/cards/getCards.js
+++ api/routes/cards/getCards.js
@@ -1,9 +1,10 @@
 const CardController = require('../../controllers/cardsController')
 
 module.exports = (req, res) => {
-    CardController.getCards().then((data) => {
+    return CardController.getCards().then((data) => {
         res.status(200).json(data)
     }).catch((err) => {
+        console.log(err)
         res.status(err.code).json(err.message)
     })
 }
