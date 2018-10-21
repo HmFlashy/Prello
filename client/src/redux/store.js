820fcb71b4dbd47aa52e718529e6bdfaf201b090
Hugo Maitre <hugo.maitre69@gmail.com>
2018-10-21 11:39:50 +0200

    Fix react dev tools optional

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,10 +4,13 @@ import rootReducer from './reducers/RootReducer';
 import socketIO from '../services/SocketService'
 
 export default function configureStore(initialState={}) {
- const store = createStore(
-  rootReducer, 
-  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) 
- );
- socketIO.init(store)
- return store
+    let middlewares = [applyMiddleware(thunk)]
+    if(window.__REDUX_DEVTOOLS_EXTENSION__) 
+        middlewares.push(window.__REDUX_DEVTOOLS_EXTENSION__())
+    const store = createStore(
+    rootReducer, 
+    compose(middlewares) 
+    );
+    socketIO.init(store)
+    return store
 }
