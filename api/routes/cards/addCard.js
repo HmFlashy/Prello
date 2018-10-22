2241714ee5eb54e923912a01e0d5bfb960a79aed
Rifhice <giordani.kevin.02@gmail.com>
2018-10-22 23:16:02 +0200

    card api

--- api/routes/cards/addCard.js
+++ api/routes/cards/addCard.js
@@ -1,17 +1,22 @@
 const CardController = require('../../controllers/CardsController')
 const socketIO = require('../../../socket')
+const throwError = require('../../helper/RequestHelper').throwError;
+
 
 module.exports = async (req, res) => {
-    const name = req.body.name;
-    const listId = req.body.listId;
     try {
+        const name = req.body.name;
+        const listId = req.body.listId;
+        if (!listId || !listId.match(/^[0-9a-fA-F]{24}$/)) {
+            throwError(400, "Bad Request listId malformed")
+        }
         const card = await CardController.addCard(name, listId)
         socketIO.broadcast('action', {
             type: 'ADD_CARD',
             payload: card
         })
         return res.status(200).json(card)
-    } catch(error) {
+    } catch (error) {
         res.status(500).json(error.message)
     }
 }
--- api/routes/cards/index.js
+++ api/routes/cards/index.js
@@ -5,6 +5,17 @@ const router = express.Router();
 router.post('/', require('./addCard'));
 router.get('/', require('./getCards'));
 router.get('/:idCard', require('./getCardById'));
-router.put('/:idCard', require('./updateCardName'));
+router.put('/:idCard', require('./updateCard'));
+
+router.post('/:idCard/:field', require('./arrays/addValToField'));
+router.delete('/:idCard/:field', require('./arrays/removeValToField'));
+
+module.exports = router;
+
+
+
+
+
+
 
 module.exports = router;
