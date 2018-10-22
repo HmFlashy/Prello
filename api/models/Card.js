41bb6608b1c9d442e2fac7262b567c8f1ef35626
Loris Zirah <loris.zirah@etu.umontpellier.fr>
2018-10-22 19:29:17 +0200

    Added summary in card model

--- api/models/Card.js
+++ api/models/Card.js
@@ -1,12 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const cardInformation = new mongoose.Schema({
+    nbComments: {type: Number, default: 0},
+    nbItems: {type: Number, default: 0},
+    nbItemsChecked: {type: Number, default: 0},
+    nbAttachments: {type: Number, default: 0},
+    description: {type: String, default: false}
+});
 
 const Item = new mongoose.Schema({
     name: {type: String, required: true},
     isChecked: {type: Boolean, default: false}
-},{timestamps: true});
+}, {timestamps: true});
 
 const CheckList = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: {type: String, required: true},
     items: {
         type: [Item],
         default: []
@@ -14,24 +22,26 @@ const CheckList = new mongoose.Schema({
 }, {timestamps: true});
 
 const Card = new mongoose.Schema({
-    name: { type: String },
+    name: {type: String},
     desc: String,
     dueDate: Date,
     dueDateCompleted: Date,
     isArchived: {type: Boolean, default: false},
-    creator: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    labels: [{type: mongoose.Schema.Types.ObjectId, ref: 'Label'}],
-    comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
-    members: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    attachments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Attachment'}],
+    creator: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    labels: [{type: mongoose.Schema.Types.ObjectId, ref: "Label"}],
+    comments: [{type: mongoose.Schema.Types.ObjectId, ref: "Comment"}],
+    members: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    attachments: [{type: mongoose.Schema.Types.ObjectId, ref: "Attachment"}],
     checklists: [CheckList],
-    watchers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    list: {type: mongoose.Schema.Types.ObjectId, ref: 'List'},
-    board: {type: mongoose.Schema.Types.ObjectId, ref: 'Board'},
+    watchers: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    list: {type: mongoose.Schema.Types.ObjectId, ref: "List"},
+    board: {type: mongoose.Schema.Types.ObjectId, ref: "Board"},
     pos: Number,
-    activities: [{type: mongoose.Schema.Types.ObjectId, ref: 'Action'}]
+    activities: [{type: mongoose.Schema.Types.ObjectId, ref: "Action"}],
+    information: [cardInformation]
 }, {timestamps: true});
 
-module.exports = mongoose.model('Item', Item);
-module.exports = mongoose.model('CheckList', CheckList);
-module.exports = mongoose.model('Card', Card);
+module.exports = mongoose.model("Item", Item);
+module.exports = mongoose.model("CheckList", CheckList);
+module.exports = mongoose.model("Card", Card);
+module.exports = mongoose.model("cardInformation", cardInformation);
