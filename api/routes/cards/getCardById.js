11493c7d592d560624ae79cf66b6c79b407c939a
Kevin giordani <giordani.kevin.02@gmail.com>
2018-10-23 18:56:53 +0200

    Documentation

--- api/routes/cards/getCardById.js
+++ api/routes/cards/getCardById.js
@@ -1,5 +1,39 @@
 const CardController = require('../../controllers/CardsController')
 
+/**
+  * @swagger
+  * definition:
+  *   NewList:
+  *     properties:
+  *       name:
+  *         type: string
+  *
+  * paths:
+  *   /cards/{cardId}:
+  *     get:
+  *       tags:
+  *         - Card
+  *       description: Fetches the card with the specified Id
+  *       summary: Fetches the card with the specified Id
+  *       parameters:
+  *         - name: cardId
+  *           schema:
+  *             type: string
+  *           description: The id of the card to fetch
+  *           in: path
+  *           required: true
+  *       responses:
+  *         200:
+  *           description: All the cards
+  *           content:
+  *             application/json:
+  *               schema:
+  *                 $ref: '#components/schemas/Card'
+  *         400:
+  *           description: The request was malformed
+  *         500:
+  *           description: Internal error
+  */
 module.exports = (req, res) => {
     console.log(req.params)
     return CardController.getCardById(req.params.idCard).then((data) => {
