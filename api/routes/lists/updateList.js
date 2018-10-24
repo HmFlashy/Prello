ad3acda932d39acf2430073e70311ce158af1c09
Loris Zirah <loris.zirah@etu.umontpellier.fr>
2018-10-24 20:09:15 +0200

    Documentation of the routes
    add delete and update list

--- api/routes/lists/updateList.js
+++ api/routes/lists/updateList.js
@@ -2,37 +2,90 @@ const ListController = require('../../controllers/ListsController');
 const socketIO = require('../../../socket');
 const throwError = require('../../helper/RequestHelper').throwError;
 
+const types = {
+    name: 'UPDATE_LIST_NAME',
+    isArchived: 'UPDATE_LIST_ISARCHIVED',
+    boardId: 'UPDATE_LIST_BOARD'
+};
+
+/**
+ * @swagger
+ * definition:
+ *   NewList:
+ *     properties:
+ *       name:
+ *         type: string
+ *
+ * paths:
+ *   /lists/{listId}:
+ *     put:
+ *       tags:
+ *         - List
+ *       description: Update attributes of the given list. Attributes can be => name, pos, boardId, archive. You can change any number of those fields in one call.
+ *       summary: Update attributes of the given list.
+ *       parameters:
+ *         - name: listId
+ *           schema:
+ *             type: string
+ *           description: The id of the list to update
+ *           in: path
+ *           required: true
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                 board:
+ *                   type: ObjectId
+ *                 pos:
+ *                   type: int
+ *                 archive:
+ *                   type: boolean
+ *             example:
+ *               name: my super name
+ *               isArchived: true
+ *               boardId: 5bce3aaf84c77d0a433029a9
+ *               pos: 100000
+ *       responses:
+ *         200:
+ *           description: The updated list
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#components/schemas/List'
+ *         400:
+ *           description: The request was malformed.
+ *         404:
+ *           description: The list was not found.
+ *         500:
+ *           description: Internal error.
+ */
 module.exports = async (req, res) => {
     try {
-        if(!req.params.listId) {
-            throwError(400, "Bad Request", "Missing listId parameter")
-        }
         const listId = req.params.listId;
+        if(!listId) {
+            throwError(400, "Missing listId parameter")
+        }
+        if(!listId.match(/^[0-9a-fA-F]{24}$/)) {
+            throwError(400, `The listId ${listId} is malformed`)
+        }
         const listUpdated = await ListController.updateList(listId, req.body);
         if(!listUpdated) {
-            throwError(400, "List not found")
+            throwError(400, `The listId ${listId} does not exist`)
         }
-        switch (req.body) {
-            case 'name' in Object.keys(req.body):
+        Object.keys(req.body).forEach(action => {
+            if (types[action]) {
                 socketIO.broadcast('action', {
-                    type: 'UPDATE_LIST_NAME',
-                    payload: listUpdated
-                });
-                break;
-            case 'isArchived' in Object.keys(req.body):
-                socketIO.broadcast('action', {
-                    type: 'ARCHIVED_LIST',
-                    payload: listUpdated
-                });
-                break;
-            default:
-                break;
-        }
-        return res.status(200).json({
-            type: "Success",
-            message: "List updated",
-            data: listUpdated
-        })
+                    type: types[action],
+                    payload: { [action]: listUpdated[action], "_id": listUpdated._id }
+                })
+            }
+        });
+        return res.status(200).json(listUpdated)
     } catch(error) {
         console.log(error)
         if(error.code){
