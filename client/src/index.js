ed89e561562cd76a169e8cea6539fb1f7a5f913a
Kevin giordani <giordani.kevin.02@gmail.com>
2018-10-24 13:03:49 +0200

    fix provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
-import configureStore from './redux/store'
 import 'semantic-ui-css/semantic.min.css';
 import 'normalize.css'
 
 ReactDOM.render(
-    <Provider store={configureStore()}>
-        <App />
-    </Provider>, 
- document.getElementById('root'));
+    <App />,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
