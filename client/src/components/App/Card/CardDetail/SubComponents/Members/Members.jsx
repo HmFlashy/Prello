29fb7ecccd18bc8e87f79024679633150403b746
Rifhice <giordani.kevin.02@gmail.com>
2018-10-28 15:58:56 +0100

    add avatar to card detail [need populate from back]

--- client/src/components/App/Card/CardDetail/SubComponents/Members/Members.jsx
+++ client/src/components/App/Card/CardDetail/SubComponents/Members/Members.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Members.css'
 import { Icon } from 'semantic-ui-react'
+import Avatar from 'react-avatar';
 
 export default (props) => (
     <div className={props.className}>
         <p>Members</p>
+        {props.members ? props.members.map(member =>
+            console.log(member) || <Avatar key={member._id} name={member.fullName} round size="25" textSizeRatio={1.4}></Avatar>
+        ) : ""}
         <Icon name='plus' />
     </div>
 )
