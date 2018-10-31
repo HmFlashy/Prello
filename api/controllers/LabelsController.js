b04359179e2cae0b651f3a26fb7a612b921e5119
Alex Aufauvre <alex.aufauvre@etu.umontpellier.fr>
2018-10-31 15:40:32 +0100

    Backend Done

--- api/controllers/LabelsController.js
+++ api/controllers/LabelsController.js
@@ -1,6 +1,7 @@
 const Board = require('../models').Board;
 const Label = require('../models').Label;
 const Card = require('../models').Card;
+const BoardsController = require('./BoardsController')
 const mongoose = require('mongoose');
 const throwError = require('../helper/RequestHelper').throwError;
 
@@ -23,8 +24,7 @@ const createLabel = async (name, color, boardId) => {
             board: boardId
         });
         const savedLabel= await label.save()
-        await Board.findOneAndUpdate({ _id: boardId },
-            { $push: { labels: savedLabel } }, { "new": true })
+        await BoardsController.addLabel(boardId, savedLabel)
         return savedLabel
     } catch (error) {
         throw error
@@ -40,11 +40,10 @@ const deleteLabel = async (boardId, labelId) => {
         if(!label) {
             throwError(404, `The label ${labelId} was not found`)
         }
-        deleteLabelFromBoard(boardId, labelId);    
-        label.remove();
+        await BoardsController.removeLabel(boardId,labelId);    
         await session.commitTransaction();
         session.endSession();
-        return label
+        return await label.remove();
     } catch (error) {
         console.log(error)
         await session.abortTransaction();
@@ -53,14 +52,6 @@ const deleteLabel = async (boardId, labelId) => {
     }
 }
 
-const deleteLabelFromBoard = async (boardId, labelId) => {
-    try {
-        return await Board.findOneAndUpdate({ _id: boardId },
-        { $pull: { labels: { _id: labelId } } }, { "new": true })
-    } catch (error) {
-        throw error
-    }
-}
 
 const updateLabel = async (labelId, data) => {
     try {
