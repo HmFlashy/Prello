bfd300b6a9f589346e453df0ca0dbc88cabd2250
Alex Aufauvre <alex.aufauvre@etu.umontpellier.fr>
2018-11-12 11:59:41 +0100

    Routes create & add board

--- server/api/controllers/TeamsController.js
+++ server/api/controllers/TeamsController.js
@@ -1,13 +1,51 @@
-const Team = require('../models/index').Team;
-const throwError = require('../helper/RequestHelper').throwError;
+const Board = require("../models/index").Board;
+const User = require("../models/index").User;
+const Team = require("../models/index").Team;
+const Category = require("../models/index").Category;
+const throwError = require("../helper/RequestHelper").throwError;
+const mongoose = require("mongoose");
+
+const addTeam = async (name, creatorId) => {
+
+    try {
+        const team = new Team({
+            name: name,
+            creator: creatorId
+        });
+        const savedTeam= await team.save()
+        return savedTeam
+    } catch (error) {
+        throw error
+    }
+}
+
+
+const addToArray = async (teamId, key, data) => {
+    try {
+        return await Team.findOneAndUpdate({ _id: teamId },
+            { $push: { [key]: data } }, { "new": true })
+    } catch (error) {
+        throw error
+    }
+}
+
+const removeToArray = async (teamId, key, data) => {
+    try {
+        console.log(data)
+        return await Team.findOneAndUpdate({ _id: teamId },
+            { $pull: { [key]: data } }, { "new": true })
+    } catch (error) {
+        throw error
+    }
+}
 
 const getTeamById = async (teamId) => {
     try {
         const team = await Team.findById(teamId).populate({
             path: "members.member",
             select: ["fullName", "username", "initials", "_id", "bio"]
-        });
         if(!team){
+        });
             throwError(404, "Team not found")
         }
         return team;
@@ -15,7 +53,9 @@ const getTeamById = async (teamId) => {
         throw error
     }
 };
-
 module.exports = {
+    addTeam,
+    addToArray,
+    removeToArray
     getTeamById
 };
