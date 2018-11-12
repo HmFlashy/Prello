b8972777f80de7cb2d59a4c63a6e638e4669a02f
Alex Aufauvre <alex.aufauvre@etu.umontpellier.fr>
2018-11-12 12:50:09 +0100

    Routes index

--- server/api/routes/privateRoutes/teams/index.js
+++ server/api/routes/privateRoutes/teams/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+router.post('/teams', require('../teams/addTeam'));
+router.delete('/teams/:teamId', require('../teams/deleteTeam'));
+router.put('/:teamId/boards/:boardId', require('../teams/addTeamBoard'));
+router.delete('/:teamId/boards/:boardId', require('../teams/deleteTeamBoard'));
 router.get('/:teamId', require('./getTeamById'));
 
 module.exports = router;
