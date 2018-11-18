6cadb2e69d8edd28cc94757f3db54f9dfdb0a94a
Rifhice <giordani.kevin.02@gmail.com>
2018-11-18 22:16:35 +0100

    fix sonarqube

--- client/src/containers/BoardContainer/BoardHeaderContainer.js
+++ client/src/containers/BoardContainer/BoardHeaderContainer.js
@@ -174,7 +174,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
             try {
                 await BoardServices.changeRoleBoardMember(ownProps.board._id, memberId, role);
             } catch (error) {
-                throw error
+                console.log(error)
             }
         },
         async removeMember(memberId) {
@@ -182,7 +182,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
                 await BoardServices.deleteMember(ownProps.board._id, memberId);
             } catch (error) {
                 dispatch(actionFailedDeletingBoardMember(error))
-                throw error
+                console.log(error)
             }
         },
         async removeTeam(teamId) {
@@ -190,7 +190,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
                 await BoardServices.deleteTeam(ownProps.board._id, teamId);
             } catch (error) {
                 dispatch(actionFailedDeletingBoardMember(error))
-                throw error
+                console.log(error)
             }
         },
         async deleteBoard() {
@@ -198,7 +198,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
                 await BoardServices.deleteBoard(ownProps.board._id);
             } catch (error) {
                 dispatch(actionFailedDeletingBoardMember(error))
-                throw error
+                console.log(error)
             }
         }
     }
